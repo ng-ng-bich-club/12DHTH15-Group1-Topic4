94cb70161dd742e8028934446b6f4a53517b75fe
Nguyễn Công Dũng <97574248+CongDungDJ@users.noreply.github.com>
2022-01-22 16:25:27 +0700

    Cập nhật Phần 2 - Mảng cấu trúc
    Mô tả này để mô tả những mô tả cần mô tả để có thể mô tả lại mọi mô tả mà các mô tả cần mô tả

--- Phần 2 - Mảng Cấu Trúc/Phần 2 - Mảng Cấu Trúc.cpp
+++ Phần 2 - Mảng Cấu Trúc/Phần 2 - Mảng Cấu Trúc.cpp
@@ -112,6 +112,62 @@ void displayAllPrescriptions(Prescription prescription[], int nPrescription) {
 
 }
 
+//Cau 6:
+void sortprescriptionascendingtotalmoney(Prescription prescription[], int n)
+{
+	cout << "Danh sach don thuoc theo thu tu tang dan tong tien." << endl << endl;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i+1; j < n; j++)
+		{
+			if (prescription[i].TotalMoney > prescription[j].TotalMoney)
+			{
+				swap(prescription[i], prescription[j]);
+			}
+		}
+	}
+}
+//Cau 7
+void listboughtprescriptionpatient(Prescription prescription[], int n)
+{
+	char name[30];
+	int res = 0;
+	cout << "Nhap ten khach hang can do tim: "; fflush(stdin); gets_s(name);
+	//titlelist(prescription,n); // Tiêu đề 
+	for (int i = 0; i < n; i++)
+	{
+		if (prescription[i].Patient == name)
+		{
+			displayAPrescription(prescription[i]);
+			res++;
+		}
+	}
+	if (res == 0)
+	{
+		cout << "Ten khach hang khong ton tai" << endl;
+	}
+}
+//Câu 8
+void changenamedocterofpatient(Prescription prescription[], int n)
+{
+	char name[30];
+	int res = 0;
+	cout << "Nhap ten khach hang can do tim: "; fflush(stdin); gets_s(name);
+	//titlelist(prescription, n); // Tiêu đề 
+	for (int i = 0; i < n; i++)
+	{
+		if (prescription[i].Patient == name)
+		{
+			cout << "Nhap ten bac si moi: "; cin >> prescription[i].Doctor;
+			displayAPrescription(prescription[i]);
+			res++;
+		}
+	}
+	if (res == 0)
+	{
+		cout << "Ten khach hang khong ton tai" << endl;
+	}
+}
 //Ham menu
 void Menu() {
     cout << "Welcome to Menu !" << endl;
@@ -121,9 +177,9 @@ void Menu() {
     cout << "3.\tTinh gia tri trung binh cong cua 4 bien/duong cheo chinh/duong cheo phu" << endl;
     cout << "4.\t" << endl;
     cout << "5.\t" << endl;
-    cout << "6.\t" << endl;
-    cout << "7.\t" << endl;
-    cout << "8.\t" << endl;
+    cout << "6.\tSap xep cac don thuoc tang dan theo tong tien." << endl;
+   	cout << "7.\tHay liet ke cac don thuoc da ban cho khach hang ten la X." << endl;
+   	cout << "8.\tDieu chinh lai ten cua bac si trong don thuoc da ban cho benh nhan co ten la X." << endl;
     cout << "9.\t" << endl;
     cout << "10.\tTu ma tran A tao ma tran F duoc dich phai xoay vong cac cot theo truc dung voi chieu tu trai sang phai" << endl;
     cout << "11.\tTu ma tran A tao va xuat ra mot ma tran I duoc xoan oc giam dan nguoc chieu kim dong ho" << endl;
@@ -186,12 +242,16 @@ int main() {
     //        break;
     //    case 5:
     //        break;
-    //    case 6:;
-    //        break;
-    //    case 7:
-    //        break;
-    //    case 8:
-    //        break;
+          case 6:
+			       sortprescriptionascendingtotalmoney(A, 2);
+		       	displayAllPrescriptions(A, 2);
+	             break;
+	         case 7:
+		       	listboughtprescriptionpatient(A, 2);
+	             break;
+	         case 8:
+			       changenamedocterofpatient(A, 2);
+	             break;
     //    case 9:
     //        break;
     //    case 10:
